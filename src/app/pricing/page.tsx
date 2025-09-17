11a83db6ead47abadcf68978a7cb0a2be1b050f5
Max Goh <themaxgohjh@gmail.com>
2025-09-18 01:11:29 +0800

    Remove pricing

--- src/app/pricing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PricingSection } from "@/components/pricing";
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-background via-card to-secondary/30">
-      <PricingSection />
-    </main>
-  );
-}
