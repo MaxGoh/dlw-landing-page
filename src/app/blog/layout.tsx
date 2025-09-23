ee7231ec8fda08a96c13a44da1c82887d0b632c6
Max Goh <themaxgohjh@gmail.com>
2025-09-23 08:18:25 +0800

    improve blog layout

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -14,7 +14,7 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       {children}
     </div>
   );
